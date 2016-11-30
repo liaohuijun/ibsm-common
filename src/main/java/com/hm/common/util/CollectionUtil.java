b4983ceb665052842d6524f94c68a76d29509a52
wangshishun <cd_shishun156@163.com>
2016-11-30 14:29:21 +0800

    [ADD]集合工具类引入

---
+++ src/main/java/com/hm/common/util/CollectionUtil.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.hm.common.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author shishun.wang
+ * @date 2016年11月28日 上午10:14:07
+ * @version 1.0
+ * @describe
+ */
+public class CollectionUtil extends CommonUtil {
+
+	/**
+	 * @param collection
+	 * @return false:当前集合不为空并且元素长度不等于0
+	 */
+	public static boolean isCollectionBlank(List<?> collection) {
+		if (isEmpty(collection)) {
+			return true;
+		}
+		return collection.size() == 0;
+	}
+
+	@SafeVarargs
+	public static final <T> Set<T> toSet(T... paramm) {
+		HashSet<T> set = new HashSet<>(paramm.length);
+		for (T parameter : paramm) {
+			set.add(parameter);
+		}
+		return set;
+	}
+
+	public static final <T> List<T> distinct(List<T> list, Comparator<T> comparator) {
+		if (isEmpty(list)) {
+			return Collections.emptyList();
+		}
+		List<T> targetList = new ArrayList<>();
+		list.stream().filter(item -> item != null).forEach(item -> {
+			Optional<T> tryFind = targetList.parallelStream()
+					.filter(targetItem -> comparator.compare(item, targetItem) == 0).findAny();
+			if (!tryFind.isPresent()) {
+				targetList.add(item);
+			}
+		});
+		return targetList;
+	}
+
+	public static final <T> List<T> distinct(List<T> list) {
+		if (isEmpty(list)) {
+			return Collections.emptyList();
+		}
+		return list.stream().distinct().collect(Collectors.toList());
+	}
+}
