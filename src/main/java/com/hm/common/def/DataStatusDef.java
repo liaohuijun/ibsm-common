7d338a11ee6e17e137befef2198783249df9ec76
shishun.wang <1058300241@qq.com>
2017-02-02 00:20:48 +0800

    [add] add  db data delete row status

---
+++ src/main/java/com/hm/common/def/DataStatusDef.java
@@ -0,0 +1,20 @@
+package com.hm.common.def;
+
+/**
+ * @author shishun.wang
+ * @date 下午10:41:28 2017年2月1日
+ * @version 1.0
+ * @describe 数据状态
+ */
+public enum DataStatusDef {
+
+	/**
+	 * 禁用（已删除）
+	 */
+	DISABLE,
+
+	/**
+	 * 启用（正在使用）
+	 */
+	ENABLE;
+}
