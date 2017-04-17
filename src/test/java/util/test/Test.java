ab4ac9ce19ce1401d0738aee44f56cc9fc3497ad
shishun.wang <wangshishun@sm.vvip-u.com>
2017-04-17 16:15:53 +0800

    [add] jsonp 网络抓取页面

---
+++ src/test/java/util/test/Test.java
@@ -0,0 +1,23 @@
+package util.test;
+
+/**
+ * @author shishun.wang
+ * @date 2017年4月17日 下午3:22:22
+ * @version 1.0
+ * @describe
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(x(370, 100, 80));
+		System.out.println(pay(205, 100, 80));
+	}
+
+	public static double pay(double x, double l, double r) {
+		return x + (x / l) * r;
+	}
+
+	public static double x(double p, double l, double r) {
+		return (p * l) / (l + r);
+	}
+}
