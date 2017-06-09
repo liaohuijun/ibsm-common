7a0ff24775dcd3eedbcb7b229f4000eabf1dae92
shishun.wang <wangshishun@sm.vvip-u.com>
2017-06-09 14:51:18 +0800

    [optimize] 优化单元测试用例

--- src/test/java/jxab/Test.java
+++ src/test/java/jxab/Test.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 
+import com.hm.common.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,34 +13,34 @@ import com.hm.common.util.CommonUtil;
 
 /**
  * @author shishun.wang
- * @date 2017年3月23日 下午3:39:38
  * @version 1.0
+ * @date 2017年3月23日 下午3:39:38
  * @describe
  */
 public class Test {
 
-	private static Logger logger = LoggerFactory.getLogger(Test.class);
-	
-	public static void main(String[] args) {
-		try {
-			JxabModel model = new JxabModel();
-			{
-				model.setId(UUID.randomUUID().toString());
-				model.setName("Tom");
-				model.setAge(25);
-			}
+    private static Logger logger = LoggerFactory.getLogger(Test.class);
 
-			JAXBContext jaxbContext = JAXBContext.newInstance(JxabModel.class);
-			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+    public static void main(String[] args) {
+        try {
+            JxabModel model = new JxabModel();
+            {
+                model.setId(UUID.randomUUID().toString());
+                model.setName("Tom");
+                model.setAge(25);
+            }
 
-			// output pretty printed
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            JAXBContext jaxbContext = JAXBContext.newInstance(JxabModel.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
-			jaxbMarshaller.marshal(model, System.out);
-			
-			System.out.println(CommonUtil.isPhoneLegal("135884507258"));
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-	}
+            // output pretty printed
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            jaxbMarshaller.marshal(model, System.out);
+
+            System.out.println(StringUtil.isPhoneLegal("135884507258"));
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
 }
--- src/test/java/util/test/TestAes.java
+++ src/test/java/util/test/TestAes.java
@@ -1,28 +1,29 @@
 package util.test;
 
-import com.hm.common.util.CommonUtil;
 import com.hm.common.util.EncryptUtil;
 import com.hm.common.util.StringUtil;
 
 /**
  * @author shishun.wang
- * @date 2017年4月28日 下午4:30:59
  * @version 1.0
+ * @date 2017年4月28日 下午4:30:59
  * @describe
  */
 public class TestAes {
 
-	public static void main(String[] args) {
-		String content = StringUtil.token() + ":" + System.currentTimeMillis();
-		System.out.println(content);
-		String token = EncryptUtil.AES.encrypt(content);
-		System.out.println(token);
-		System.out.println(token.length());
-		String decContent = EncryptUtil.AES.decrypt(token);
-		System.out.println(decContent);
-		System.out.println(EncryptUtil.AES.validation(content, token));
+    public static void main(String[] args) {
+        String content = StringUtil.token() + ":" + System.currentTimeMillis();
+        System.out.println(content);
+        String token = EncryptUtil.AES.encrypt(content);
+        System.out.println(token);
+        System.out.println(token.length());
+        String decContent = EncryptUtil.AES.decrypt(token);
+        System.out.println(decContent);
+        System.out.println(EncryptUtil.AES.validation(content, token));
 
-		System.out.println(CommonUtil.isNumberic("1234567890"));
+        System.out.println(StringUtil.isNumberic("1234567890"));
 
-	}
+        System.out.printf(EncryptUtil.AES.encrypt("nihao123:"+System.currentTimeMillis()));
+
+    }
 }
